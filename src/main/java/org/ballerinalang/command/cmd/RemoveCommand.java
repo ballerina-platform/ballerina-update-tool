164bb79d52edcec630ea3a0217c9195f1a8ce6c7
Suganya <suganyasuven@gmail.com>
2021-06-05 23:10:22 +0530

    Apply review suggestion

--- src/main/java/org/ballerinalang/command/cmd/RemoveCommand.java
+++ src/main/java/org/ballerinalang/command/cmd/RemoveCommand.java
@@ -126,6 +126,7 @@ public class RemoveCommand extends Command implements BCommand {
             File folder = new File(ToolUtil.getDistributionsPath());
             File[] listOfFiles;
             listOfFiles = folder.listFiles();
+            // checking for 2 files for zip pack and 3 files for installers
             if (listOfFiles.length == 2 || (listOfFiles.length == 3 && folder.toPath().resolve("installer-version").
                     toFile().exists())) {
                 getPrintStream().println("There is nothing to remove. Only active distribution is remaining");
